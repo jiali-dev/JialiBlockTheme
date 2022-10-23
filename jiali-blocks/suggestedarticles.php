d1650b31ac41f67e31d634b8a95d0003dade9122
jiali <mahyarerad@gmail.com>
2022-10-23 21:55:43 +0300

    Improve Suggested Articles Section

--- jiali-blocks/suggestedarticles.php
+++ jiali-blocks/suggestedarticles.php
@@ -4,7 +4,32 @@
         <h1 class="jiali-title">
             <?php _e( "Our suggestions", "jiali" ) ?>
         </h1>
-        Need to continue ,,,,
+        <div class="row">
+
+            <div class="col-md-6">
+
+                <div class="jiali-card jiali-vertical-card">
+                    <img class="jiali-card-img-top" src="http://jialibluetheme.local/wp-content/uploads/2018/11/Doctor-baby.jpg" alt="Card image cap">
+                    <div class="jiali-card-body">
+                        <h5 class="jiali-card-title">Card title</h5>
+                        <p class="jiali-card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="jiali-card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+            <div class="card mb-3">
+                <img class="card-img-top" src="http://jialibluetheme.local/wp-content/uploads/2018/11/Doctor-baby.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+                <p>z</p>
+                <p>h</p>
+            </div>
+        </div>
     </div>
 
 </section>
